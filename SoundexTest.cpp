4fdb661fe50b8043c8804e020a0578d7d7ff2382
Anh Nguyen <anh.nt@raksul.com>
2026-09-10 21:20:41 +0700

    (refactor): Split the logic to h file and replace namespace for testing

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,36 +1,17 @@
-#include <string>
-
-
-class Soundex {
-public:
-  std::string encode(const std::string& word) const {
-    return zeroPad(word);
-  }
-
-private:
-  std::string zeroPad(const std::string& word) const {
-    return word + "000";
-  }
-
-};
-
+#include "Soundex.h"
 #include "gmock/gmock.h"
 
-using ::testing::Eq;
+using namespace testing;
 
-class SoundexEncoding: public testing::Test {
+class SoundexEncoding: public Test {
 public:
   Soundex soundex;
 };
 
 TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWorld) {
-  auto encoded = soundex.encode("A");
-
-  ASSERT_THAT(encoded, Eq("A000"));
+  ASSERT_THAT(soundex.encode("A"), Eq("A000"));
 };
 
 TEST_F(SoundexEncoding, RetainsSoleLetterOfThreeDigits) {
-  auto encoded = soundex.encode("I");
-
-  ASSERT_THAT(encoded, Eq("I000"));
+  ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 };
